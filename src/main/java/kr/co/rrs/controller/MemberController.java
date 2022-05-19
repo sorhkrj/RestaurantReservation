026686155f63eef3fede425c8d92661356d852b7
Uue <37442691+sorhkrj@users.noreply.github.com>
2022-05-19 17:23:33 +0900

    0519 1720 End3
    master controller update

--- src/main/java/kr/co/rrs/controller/MemberController.java
+++ src/main/java/kr/co/rrs/controller/MemberController.java
@@ -1,54 +1,71 @@
 package kr.co.rrs.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.co.rrs.service.MemberService;
+import kr.co.rrs.vo.MemberVO;
 
 @Controller
 	public class MemberController {
-		//회원가입
-		@GetMapping("/memberInsert") 
-		public String memberInsert() {
-			return "member/memberInsert";
-		}
-		//지점장 회원가입
-		@GetMapping("/memberInsertEnterprise")
-		public String memberInsertEnterprise() {
-			return "member/memberInsertEnterprise";
-		}
-		//회원검색결과
-		@GetMapping("/resultMember")
-		public String resultMember() {
-			return "member/resultMember";
-		}
-		//지점검색결과
-		@GetMapping("/resultStore")
-		public String resultStore() {
-			return "member/resultStore";
-		}
-		//회원검색
-		@GetMapping("/searchMember")
-		public String seachMember() {
-			return "member/searchMember";
-		}
-		//지점검색
-		@GetMapping("/searchStore")
-		public String searchStore() {
-			return "member/searchStore";
-		}
-		//내정보
-		@GetMapping("/memberSelect")
-		public String memberSelect() {
-			return "member/memberSelect";
-		}
-		//회원탈퇴
-		@GetMapping("/memberDeleteCheck")
-		public String memberDeleteCheck() {
-			return "member/memberDeleteCheck";
-		}
-		//회원수정
-		@GetMapping("/memberUpdateCheck")
-		public String memberUpdateCheck() {
-			return "member/memberUpdateCheck";
-		}
 
+	@Autowired
+	MemberService memberService;
+	
+	//회원가입
+	@GetMapping("/memberInsert") 
+	public String memberInsert() {
+		return "member/memberInsert";
 	}
+	//내정보
+	@GetMapping("/memberSelect")
+	public String memberSelect(Model model) {
+		model.addAttribute("memberVO", memberService.selectOne("yoon"));
+		return "member/memberSelect";
+	}
+	@GetMapping("/memberInsertPro")
+	public String memberInsertPro(MemberVO memberVO) {
+		memberService.insert(memberVO);
+		return "forward:memberSelect";
+	}
+	//지점장 회원가입
+	@GetMapping("/memberInsertEnterprise")
+	public String memberInsertEnterprise() {
+		return "member/memberInsertEnterprise";
+	}
+	//회원검색결과
+	@GetMapping("/resultMember")
+	public String resultMember() {
+		return "member/resultMember";
+	}
+	//지점검색결과
+	@GetMapping("/resultStore")
+	public String resultStore() {
+		return "member/resultStore";
+	}
+	//회원검색
+	@GetMapping("/searchMember")
+	public String seachMember() {
+		return "member/searchMember";
+	}
+	//지점검색
+	@GetMapping("/searchStore")
+	public String searchStore() {
+		return "member/searchStore";
+	}
+	//회원탈퇴
+	@GetMapping("/memberDeleteCheck")
+	public String memberDeleteCheck() {
+		return "member/memberDeleteCheck";
+	}
+	//회원수정
+	@GetMapping("/memberUpdateCheck")
+	public String memberUpdateCheck() {
+		return "member/memberUpdateCheck";
+	}
+
+}
