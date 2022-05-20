53846a150cbbc84aafb9f51e216aeb7175c42e33
Seongchan Ryu <longbalckryu@google.com>
2022-05-20 14:23:47 +0900

    0520 1423 중간저장

--- src/main/java/kr/co/rrs/mapper/StoreCapacityMapper.java
+++ src/main/java/kr/co/rrs/mapper/StoreCapacityMapper.java
@@ -14,13 +14,13 @@ public interface StoreCapacityMapper {
 	@Insert("insert into reservepossible values(#{storeNo}, #{day}, #{time}, #{capacity}, #{seat})")
 	void insert(ReservePossibleVO reservePossibleVO);
 
-	@Update("update reservepossible values(#{storeNo}, #{day}, #{time}, #{capacity}, #{seat})")
+	@Update("update reservepossible set time = #{time}, capacity = #{capacity}, seat = #{seat} where storeNo = #{storeNo} and day = #{day})")
 	void update(ReservePossibleVO reservePossibleVO);
 
 	@Select("select * from reservepossible where storeNo = #{storeNo} and day >= SYSDATE")
 	List<ReservePossibleVO> selectList(int storeNo);
 
-	@Select("select * from reservepossible where storeNo = #{storeNo} and day =#{day}")
+	@Select("select * from reservepossible where storeNo = #{storeNo} and day = #{day}")
 	ReservePossibleVO select(int storeNo, String day);
 
 	@Delete("delete from store where storeNo = #{storeNo} and day = #{day}")
