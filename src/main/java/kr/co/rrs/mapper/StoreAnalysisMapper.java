3253e14fe5cb918aeb9e04b37683295456fdf892
Seongchan Ryu <longbalckryu@google.com>
2022-05-24 14:45:54 +0900

    0524 1445

--- src/main/java/kr/co/rrs/mapper/StoreAnalysisMapper.java
+++ src/main/java/kr/co/rrs/mapper/StoreAnalysisMapper.java
@@ -1,7 +1,5 @@
 package kr.co.rrs.mapper;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
@@ -15,8 +13,12 @@ public interface StoreAnalysisMapper {
 	@Select("select count(*) as count, sum(people) as people, count(*)/count(distinct visitday) as dayAvg from reservation where storeNo =  #{storeNo} and visitday >= TO_DATE(#{firstDay}, 'YYYY-MM-DD HH24:MI:SS') and visitday <= TO_DATE(#{lastDay}, 'YYYY-MM-DD HH24:MI:SS')")
 	AnalysisVO countReservation(@Param("storeNo") int storeNo, @Param("firstDay") String firstDay, @Param("lastDay") String lastDay);
 
+
 	// 날짜별 예약 건수
 //	@Select("select visitday, count(*) as dayCount from reservation where storeno = #{storeNo} and visitday >= TO_DATE(#{firstDay}, 'YYYY-MM-DD HH24:MI:SS') and visitday <= TO_DATE(#{lastDay}, 'YYYY-MM-DD HH24:MI:SS') group by visitday")
 //	List<AnalysisVO> getTrend(@Param("storeNo") int storeNo, @Param("firstDay") String firstDay,	@Param("lastDay") String lastDay);
-
+	/*
+	 * 22-05-24 : 제수가 0입니다. 예외 처리
+	 *  count(*)/decode(count(distinct visitday), 0, null, count(distinct visitday))
+	 */
 }
