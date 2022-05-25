092035b1a31c820edbf1ff97e778b493bed4988f
jumki <jumki@DESKTOP-1BAR1D2>
2022-05-25 23:47:53 +0900

    0525 11:47 리뷰 관련 수정

--- src/main/java/kr/co/rrs/service/ReviewService.java
+++ src/main/java/kr/co/rrs/service/ReviewService.java
@@ -1,5 +1,13 @@
 package kr.co.rrs.service;
 
+import org.apache.ibatis.annotations.Param;
+
+import kr.co.rrs.vo.StoreVO;
+
 public interface ReviewService {
+	// 지점 번호 지점 정보 가져오기
+	StoreVO selectOne(int storeNo);
 	
+	// 지점에 대한 좋아요 수
+	int selectReivewLikeCnt(int storeNo);
 }
