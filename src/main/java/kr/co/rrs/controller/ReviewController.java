dd280c7867573f752691e5b7c60f1daa7855147b
607-03 <607-03@DESKTOP-K03RC71>
2022-05-26 17:23:19 +0900

    0526 05:22 디자인 수정 및 리뷰 좋아요 완료 / ~~~리뷰 댓글

--- src/main/java/kr/co/rrs/controller/ReviewController.java
+++ src/main/java/kr/co/rrs/controller/ReviewController.java
@@ -1,5 +1,7 @@
 package kr.co.rrs.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.rrs.service.ReviewService;
 import kr.co.rrs.vo.ReviewLikeVO;
+import kr.co.rrs.vo.ReviewVO;
 import kr.co.rrs.vo.StoreVO;
 
 @Controller
@@ -22,27 +25,52 @@ public class ReviewController {
 	}
 	
 	@RequestMapping("/storeDetailReviewMain")
-	public String storeDetailReviewMain(StoreVO storeVO, ReviewLikeVO reviewLikeVO, Model model, @Param("id") String id) {
+	public String storeDetailReviewMain(StoreVO storeVO, ReviewLikeVO reviewLikeVO, Model model, HttpSession session) {
 		storeVO = reviewService.selectOne(storeVO.getStoreNo()); // 지점 정보 검색
-		int reviewLikeCnt = reviewService.selectReivewLikeCnt(storeVO.getStoreNo()); // 지점 좋아요 수
+		String id = (String) session.getAttribute("id"); // 로그인 id 검색
 		
-		if(reviewLikeVO.getSw() == 0) {  										// 좋아요 버튼 꺼짐 상태
-			System.out.println(reviewLikeVO.getSw()); 							// 0
-			System.out.println(id);
-			System.out.println(storeVO.getStoreNo());
-			System.out.println("---------------------------------------------------");
+		if(id != null) {
+			int likeStatus = reviewService.selectReviewLikeStatus(storeVO.getStoreNo(), id); // 내가 좋아요 했는지 안했는지 확인
+			
+			if(likeStatus == 1) {
+				System.out.println(storeVO.getStoreNo());
+				System.out.println(id);
+				System.out.println(reviewLikeVO.getSw());
+				if(reviewLikeVO.getSw() == 1) {										// 좋아요 취소 누름
+					reviewService.deleteReviewLike(storeVO.getStoreNo(), id);		// 좋아요 취소
+				}
+	
+				likeStatus = reviewService.selectReviewLikeStatus(storeVO.getStoreNo(), id);
+				reviewLikeVO.setLikeStatus(likeStatus);								// 내가 좋아요 취소 기억
+				System.out.println("-----------------------삭제 완료------------------------");
+			}
+			else if (likeStatus == 0) {
+				System.out.println(storeVO.getStoreNo());
+				System.out.println(id);
+				System.out.println(reviewLikeVO.getSw());
+				if(reviewLikeVO.getSw() == 2) {										// 좋아요 누름
+					reviewService.insertReviewLike(storeVO.getStoreNo(), id);		// 좋아요 추가
+				}
+				
+				likeStatus = reviewService.selectReviewLikeStatus(storeVO.getStoreNo(), id);
+				reviewLikeVO.setLikeStatus(likeStatus);								// 내가 좋아요 추가 기억
+				System.out.println("-----------------------삽입 완료-------------------------");
+			}
+			
 		}
-		else {																	// 좋아요 버튼 켜짐 상태
-			System.out.println(reviewLikeVO.getSw()); 							// 1
-			System.out.println(id);
-			System.out.println(storeVO.getStoreNo());
-			System.out.println("---------------------------------------------------");
-		}
+		
+		reviewLikeVO.setReviewLikeCnt(reviewService.selectReviewLikeCnt(storeVO.getStoreNo())); // 지점 좋아요 수
 		
 		model.addAttribute("storeVO", storeVO);
-		model.addAttribute("reviewLikeCnt", reviewLikeCnt);
 		model.addAttribute("reviewLikeVO", reviewLikeVO);
 		
 		return "review/storeDetailReviewMain";
+	}
+	
+	@RequestMapping("/reviewInsertPro")
+	public String reviewInsertPro(ReviewVO reviewVO) {
+		reviewService.insertReview(reviewVO);
+		
+		return "redirect:/storeDetailReviewMain?storeNo=" + reviewVO.getStoreNo();
 	}
 }
